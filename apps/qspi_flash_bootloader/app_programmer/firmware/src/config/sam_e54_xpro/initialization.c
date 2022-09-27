9bc0a26666a64cf7974677d6d93bd23f781f0df4
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2022-09-27 15:02:59 +0530

    [QSPI_FLASH] Regenerated demo for qspi_flash_bootloader_app_programmer_sam_e54_xpro
    MH3-71183
    
    Signed-off-by: Jignesh Moradiya <Jignesh.Moradiya@microchip.com>

--- apps/qspi_flash_bootloader/app_programmer/firmware/src/config/sam_e54_xpro/initialization.c
+++ apps/qspi_flash_bootloader/app_programmer/firmware/src/config/sam_e54_xpro/initialization.c
@@ -124,6 +124,8 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
     NVMCTRL_Initialize( );
 
@@ -157,6 +159,7 @@ void SYS_Initialize ( void* data )
 
     NVIC_Initialize();
 
+    /* MISRAC 2012 deviation block end */
 }
 
 
