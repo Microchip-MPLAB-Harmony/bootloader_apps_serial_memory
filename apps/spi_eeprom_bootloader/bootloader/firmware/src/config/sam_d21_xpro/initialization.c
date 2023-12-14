768e5e181221faf19172ec252649916e33819759
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-12-14 16:18:22 +0530

    [SPI] Regenerated demo for spi_eeprom_bootloader_bootloader_sam_d21_xpro
    MH3-88517

--- apps/spi_eeprom_bootloader/bootloader/firmware/src/config/sam_d21_xpro/initialization.c
+++ apps/spi_eeprom_bootloader/bootloader/firmware/src/config/sam_d21_xpro/initialization.c
@@ -48,7 +48,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -80,19 +79,23 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
 // <editor-fold defaultstate="collapsed" desc="DRV_AT25 Initialization Data">
 
 /* SPI PLIB Interface Initialization for AT25 Driver */
-const DRV_AT25_PLIB_INTERFACE drvAT25PlibAPI = {
+static const DRV_AT25_PLIB_INTERFACE drvAT25PlibAPI = {
 
     /* SPI PLIB WriteRead function */
     .writeRead = (DRV_AT25_PLIB_WRITE_READ)SERCOM1_SPI_WriteRead,
 
     /* SPI PLIB Write function */
-    .write = (DRV_AT25_PLIB_WRITE)SERCOM1_SPI_Write,
+    .writeData = (DRV_AT25_PLIB_WRITE)SERCOM1_SPI_Write,
 
     /* SPI PLIB Read function */
-    .read = (DRV_AT25_PLIB_READ)SERCOM1_SPI_Read,
+    .readData = (DRV_AT25_PLIB_READ)SERCOM1_SPI_Read,
 
     /* SPI PLIB Transfer Status function */
     .isBusy = (DRV_AT25_PLIB_IS_BUSY)SERCOM1_SPI_IsBusy,
@@ -102,7 +105,7 @@ const DRV_AT25_PLIB_INTERFACE drvAT25PlibAPI = {
 };
 
 /* AT25 Driver Initialization Data */
-const DRV_AT25_INIT drvAT25InitData =
+static const DRV_AT25_INIT drvAT25InitData =
 {
     /* SPI PLIB API  interface*/
     .spiPlib = &drvAT25PlibAPI,
@@ -126,6 +129,7 @@ const DRV_AT25_INIT drvAT25InitData =
 };
 
 // </editor-fold>
+
 
 
 // *****************************************************************************
@@ -157,7 +161,7 @@ SYSTEM_OBJECTS sysObj;
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -171,6 +175,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
@@ -191,19 +196,26 @@ void SYS_Initialize ( void* data )
 
 
 
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
     sysObj.drvAT25 = DRV_AT25_Initialize(DRV_AT25_INDEX, (SYS_MODULE_INIT *)&drvAT25InitData);
 
 
 
 
+    /* MISRAC 2012 deviation block end */
     APP_Initialize();
 
 
     NVIC_Initialize();
 
+
     /* MISRAC 2012 deviation block end */
 }
-
 
 /*******************************************************************************
  End of File
