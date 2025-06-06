6dcc935a31bd7fd41cfa8e838461df22ebf683a0
Varun A V <varun.av@microchip.com>
2025-06-06 10:39:05 +0530

    [SPI_FLASH_BOOTLOADER] Regenerated demo for spi_flash_bootloader__bootloader__firmware__pic32mz_w1_curiosity.X
    MH3-113159

--- apps/spi_flash_bootloader/bootloader/firmware/src/config/pic32mz_w1_curiosity/peripheral/gpio/plib_gpio.h
+++ apps/spi_flash_bootloader/bootloader/firmware/src/config/pic32mz_w1_curiosity/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define GPIO_RA1_OutputEnable()      (TRISACLR = (1U<<1))
 #define GPIO_RA1_InputEnable()       (TRISASET = (1U<<1))
 #define GPIO_RA1_Get()               ((PORTA >> 1) & 0x1U)
+#define GPIO_RA1_GetLatch()          ((LATA >> 1) & 0x1U)
 #define GPIO_RA1_PIN                  GPIO_PIN_RA1
 
 
@@ -224,7 +225,7 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-	 uint32_t xvalue = (uint32_t)value;
+     uint32_t xvalue = (uint32_t)value;
     GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
 }
 
