528dca5cdb45452c7b98806f0a446448c96c82d0
Varun A V <varun.av@microchip.com>
2025-06-06 10:23:35 +0530

    [QSPI_FLASH_BOOTLOADER] Regenerated demo for qspi_flash_bootloader__bootloader__firmware__pic32ck_gc01_cult.X
    MH3-113131

--- apps/qspi_flash_bootloader/bootloader/firmware/src/config/pic32ck_gc01_cult/configuration.h
+++ apps/qspi_flash_bootloader/bootloader/firmware/src/config/pic32ck_gc01_cult/configuration.h
@@ -92,7 +92,7 @@ extern "C" {
 #define DRV_SST26_START_ADDRESS         (0x0U)
 #define DRV_SST26_PAGE_SIZE             (256U)
 #define DRV_SST26_ERASE_BUFFER_SIZE     (4096U)
-#define DRV_SST26_BUFF_DESC_NUMBER      (16U)
+#define DRV_SST26_BUFF_DESC_NUMBER      (1U)
 
 
 
