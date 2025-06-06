48b2034f573eb67a1a18197890a42d490e1afa2b
Varun A V <varun.av@microchip.com>
2025-06-06 10:42:36 +0530

    [QSPI_FLASH_BOOTLOADER] Regenerated demo for qspi_flash_bootloader__bootloader__firmware__pic32mz_das_sk.X
    MH3-113135

--- apps/qspi_flash_bootloader/bootloader/firmware/src/config/pic32mz_das_sk/bootloader/bootloader_common.h
+++ apps/qspi_flash_bootloader/bootloader/firmware/src/config/pic32mz_das_sk/bootloader/bootloader_common.h
@@ -109,7 +109,7 @@ Returns:
 
 Example:
     <code>
-    
+
     #define BTL_MAJOR_VERSION       3U
     #define BTL_MINOR_VERSION       7U
 
@@ -256,15 +256,15 @@ Example:
 
         appImageStartAddr = 0x00002000;
         appImageSize = 0x8000;
-        
+
 
         if (bootloader_CRCGenerate(appImageStartAddr, appImageSize) != receivedCRC)
         {
-            
+
         }
         else
         {
-            
+
         }
 
     </code>
@@ -293,7 +293,7 @@ Returns:
     None
 
 Example:
-    <code>        
+    <code>
 
         bootloader_TriggerReset();
 
